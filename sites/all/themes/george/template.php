ee7b49bf328f8b41ac6bbbfd3a48ce1b6f44f23d
jonbot <jonathan@majorrobot.com>
2014-06-16 12:03:31 -0400

    improving responsiveness

--- sites/all/themes/george/template.php
+++ sites/all/themes/george/template.php
@@ -4,3 +4,10 @@
  * @file
  * template.php
  */
+
+
+function george_preprocess_page(&$vars) { 
+	if (current_path() == "user") {
+		$vars['theme_hook_suggestions'][] = 'page__test'; 
+	} 
+}
---
+++ sites/all/themes/george/templates/page--test.tpl.php
@@ -0,0 +1,8 @@
+
+<div class="container">
+	<div id="card-parent">
+		<div id="card">
+			<img src="<?php echo url('sites/all/themes/george/images/logo_cover_circle.png');?>" class="img-responsive" />
+		</div>
+	</div>
+</div>
