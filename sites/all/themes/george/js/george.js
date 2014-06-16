9b734440bf69ff89fee300bb600f8fc4eb556dd1
jonbot <jonathan@majorrobot.com>
2014-06-16 10:37:05 -0400

    making it work on IE, changing some styles

--- sites/all/themes/george/js/george.js
+++ sites/all/themes/george/js/george.js
@@ -6,13 +6,17 @@
         var $body = $('body').addClass('closer');
         var curStar = "";
         
+        if($('html').hasClass("csstransforms3d")) {
+        	$("#card .back").show();
+        }
+        
         $stars.not('.star-fake').on('click', function(e) {
             
 			// Clicks don't continue.
 			e.stopPropagation();
 			
 			// Open or close.
-			if(!$body.hasClass("back")) {
+			if(!$body.hasClass("onBack")) {
 				Drupal.behaviors.george.flipOpen(this);
 			} else {
 				var reopen = $(this).attr("id") != curStar;
@@ -22,33 +26,18 @@
            	curStar = $(this).attr("id");
            
 		});
-        
-        
-        $('.closer').on("click", function() {
-            
-            if($body.hasClass("star-up")){
-                $(".block").fadeOut();
-                
-                $(".big").show();
-                $body.removeClass("star-up");
-                
-             
-                $('.big').animate(origDim, function(){ 
-                    $(".big").removeClass("big").addClass("col-md-2 col-md-offset-1").css("position", "relative");
-                    $stars.css("visibility", "visible");
-                });
-
-                
-            }
-        
-        });
 
     },
     
     flipOpen: function(star) {
-    	$('body').addClass("back");
-	            
-        $("#card").toggleClass('flipped');
+    	$('body').addClass("onBack");
+	    
+	    if($('html').hasClass("csstransforms3d")) {
+        	$("#card").toggleClass('flipped');
+        } else {
+        	$('#card .front').fadeOut();
+        	$("#card .back").fadeIn();
+        }
         
         var out = setTimeout(function(){
              $(".block[rel='" + $(star).attr('id') + "']").fadeIn();
@@ -58,10 +47,16 @@
 
     
     flipBack: function(reopen, newStar) {
-    	$('body').removeClass("back");
+    	$('body').removeClass("onBack");
     
         $(".block").fadeOut("fast");
-        $("#card").toggleClass('flipped');
+        
+        if($('html').hasClass("csstransforms3d")) {
+        	$("#card").toggleClass('flipped');
+        } else {
+        	$('#card .back').fadeOut();
+        	$("#card .front").fadeIn();
+        }
         
         if(reopen) {
         	var out = setTimeout(function(){
