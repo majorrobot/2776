e5418f4da8abe4ba9498c40309a182ae58f15397
Jonathan Bourland <jonathan@majorrobot.com>
2014-06-25 18:09:54 -0500

    commenting out test tpl

--- sites/all/themes/george/template.php
+++ sites/all/themes/george/template.php
@@ -8,6 +8,6 @@
 
 function george_preprocess_page(&$vars) { 
 	if (current_path() == "user") {
-		$vars['theme_hook_suggestions'][] = 'page__test'; 
+		//$vars['theme_hook_suggestions'][] = 'page__test'; 
 	} 
 }
